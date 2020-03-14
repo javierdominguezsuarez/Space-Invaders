67b1f8f0acad92374878de20b4e40affdda5445a
Francisco Javier Dominguez Suarez <56936193+javierdominguezsuarez@users.noreply.github.com>
2020-03-14 00:19:17 +0000

    Created the s

--- SpaceInvaders/spaceinvaders.py
+++ SpaceInvaders/spaceinvaders.py
@@ -4,5 +4,33 @@ from os.path import abspath, dirname
 from random import choice
 import genericdata, bullet , ship, alien, aliengroup, text, life, wall
 
-class SpaceInvaders ():
-    pass
+class SpaceInvaders (GenericData):
+
+    def __init__(self):
+        display.set_caption("Space invaders") #Configura el titulo de la ventana
+        self.screen = display.set_mode((GenericData.AREA)) #Configura el area de la pantalla
+        self.background = image.load(GenericData.BG_PATH + 'background.jpg').convert()#preparamos la imagen del background
+        self.clock=time.Clock() #iniciamos el reloj
+        
+
+    def main (self):
+        
+        self.__init__()
+        init() #pygame init
+        self.screen.blit(self.background, (0, 0))#ponemos el background
+        display.flip()#actualizamos la screen
+        
+        while True:
+            for evento in event.get(): # El usuario hizo algo
+                if evento.type == QUIT:
+                    sys.exit()
+                    
+            display.update() #actualizamos
+            self.clock.tick(60)#fps
+
+                
+            
+            
+if __name__ == '__main__':
+    game = SpaceInvaders()
+    game.main()
