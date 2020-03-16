950e20ebb86ff616dc39d46648aa2efd061501f8
Francisco Javier Dominguez Suarez <56936193+javierdominguezsuarez@users.noreply.github.com>
2020-03-16 21:03:27 +0000

    Add files via upload

--- SpaceInvaders/ejecutable.py
+++ SpaceInvaders/ejecutable.py
@@ -7,7 +7,7 @@ class GenericData ():
     """Clase con la información necesaria para el juego"""
     #Necesary routes
     DIRECT_PATH = abspath(dirname(__file__))
-    IMG_PATH = DIRECT_PATH + '/Images/'
+    IMG_PATH = DIRECT_PATH + '/Ship/'
     BG_PATH = DIRECT_PATH + '/Background/'
 
     #Colors
