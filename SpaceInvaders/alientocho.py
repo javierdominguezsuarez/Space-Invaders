2dae786acf7444732ab8e6c2df18d3da48d09ce0
Francisco Javier Dominguez Suarez <56936193+javierdominguezsuarez@users.noreply.github.com>
2020-03-25 21:10:46 +0000

    Boss alien class added

---
+++ SpaceInvaders/alientocho.py
@@ -0,0 +1,8 @@
+class AlienTocho (Alien):
+    """Clase para manejar aliens tochos"""
+    def __init__(self,speed):
+        """Método para iniciar el alien tocho"""
+        super().__init__(speed)
+        self.image = image.load(GenericData.AL_PATH +"alien_tocho.png")
+        self.rect = self.image.get_rect()
+        self.top = 500
